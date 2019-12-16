a07d9a3e7e8b90591e1726f6b31eca480d36d5d5
Macaraig <angelo.macaraig@nokia.com>
2019-12-16 16:06:25 +0800

    recommendations

--- server.py
+++ server.py
@@ -3,19 +3,23 @@ from flask import jsonify
 
 project_farm_server = Flask(__name__)
 
-rainfall = {
-    "Jan" : 3,
-    "Feb" : 5,
-    "Mar" : 5,
-    "Apr" : 6,
-    "May" : 7,
-    "Jun" : 2,
-    "Jul" : 1
-}
-
 @project_farm_server.route("/rainfall")
 def home():
+    rainfall = {
+        "Jan" : 3,
+        "Feb" : 5,
+        "Mar" : 5,
+        "Apr" : 6,
+        "May" : 7,
+        "Jun" : 2,
+        "Jul" : 1
+    }
     return jsonify(rainfall)
+
+@project_farm_server.route("/recommendations")
+def home():
+    recommendations = ["rice", "squash", "green chili" ]
+    return jsonify(recommendations)
 
 
 if __name__ == "__main__":
