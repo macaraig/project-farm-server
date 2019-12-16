b953a1498b26a8e4c6b180b02f6fe10b016727c1
Macaraig <angelo.macaraig@nokia.com>
2019-12-16 14:57:22 +0800

    trigger build

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ project_farm_server = Flask(__name__)
 @project_farm_server.route("/")
 def home():
     return "Hello, World!"
-    
+
+
 if __name__ == "__main__":
     project_farm_server.run(debug=True)
