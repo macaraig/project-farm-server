6b78dc1a43f088b2f51e80157c869f6662c9a4ca
Macaraig <angelo.macaraig@nokia.com>
2019-12-17 13:25:39 +0800

    GET /temperature

--- server.py
+++ server.py
@@ -21,6 +21,19 @@ def get_reco():
     recommendations = ["rice", "squash", "green chili" ]
     return jsonify(recommendations)
 
+@project_farm_server.route("/temperature")
+def get_temp():
+    temp = {
+        "Jan" : 25,
+        "Feb" : 29,
+        "Mar" : 32,
+        "Apr" : 40,
+        "May" : 29,
+        "Jun" : 28,
+        "Jul" : 26
+    }
+    return jsonify(temp)
+
 
 if __name__ == "__main__":
     project_farm_server.run(debug=True)
